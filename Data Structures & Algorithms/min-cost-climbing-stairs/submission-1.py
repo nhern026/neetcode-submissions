c15a11507b062878a655a68e87cb1cd35e7784ec
Nicholas Hernandez <112581921+nhern026@users.noreply.github.com>
2026-09-18 12:41:47 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,16 @@
+# Session 1, attempt 2: top down approach
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+
+        def topDown(i, memo = {}):
+            if i == 0 or i == 1:
+                return 0
+            if i in memo:
+                return memo[i]
+
+            stair_cost = min(topDown(i-2, memo) + cost[i-2], topDown(i-1, memo) + cost[i-1])
+            memo[i] = stair_cost
+            return stair_cost
+        
+        return topDown(len(cost))
