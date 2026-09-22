3e9e8f4fefc13c4f8f6e8235774177c33f7cd6f7
Nicholas Hernandez <112581921+nhern026@users.noreply.github.com>
2026-09-22 00:03:23 -0400

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.py
@@ -0,0 +1,40 @@
+# Session 1, attempt 1:
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        courseSeen = set()
+
+        # build adjacency list
+        prereqMap = {}
+        for n in range(numCourses):
+            prereqMap[n] = []
+        for crs, pre in prerequisites:
+            prereqMap[crs].append(pre)
+
+        visited = set()
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if crs in courseSeen: # *****
+                return True
+
+            visited.add(crs)
+            for pre in prereqMap[crs]:
+                if not dfs(pre):
+                    return False
+
+            # postorder - think about why this has to be after we search the prereqs
+            if crs not in courseSeen:
+                res.append(crs)
+            courseSeen.add(crs)
+
+            # prereqMap[crs] = [] don't need this anymore because of courseSeen *****
+            visited.remove(crs)
+            return True
+
+
+        for n in range(numCourses):
+            if not dfs(n):
+                return []
+        
+        return res
