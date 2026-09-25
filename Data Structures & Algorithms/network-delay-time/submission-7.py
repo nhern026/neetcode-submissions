44a0b8189195623d17d4cc345655fe114abb1ef6
Nicholas Hernandez <112581921+nhern026@users.noreply.github.com>
2026-09-25 14:01:08 -0400

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.py
@@ -0,0 +1,45 @@
+# Session 2, attempt 1: 
+# look at how i didn't inflate the heap below. 
+# i only added to it if it improved
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # build edge list
+        node2edges = defaultdict(list)
+        for src, dst, ti in times:
+            node2edges[src].append([dst, ti])
+        
+        # build min time holder 
+        node2time = {}
+        for node_id in range(n):
+            node2time[node_id+1] = math.inf
+        node2time[k] = 0
+
+        # initialize heap
+        min_time_heap = []
+        heapq.heappush(min_time_heap, [0, k])
+
+        seen = set()
+        while min_time_heap:
+            trail_time, curr = heapq.heappop(min_time_heap)
+            
+            # check if in seen
+            if curr in seen:
+                continue
+            seen.add(curr)
+
+            # update kids and add to heap
+            for dst, ti in node2edges[curr]:
+                if trail_time + ti < node2time[dst]: # only update or push if its an improvement, this helps keep queue from inflating
+                    node2time[dst] = trail_time + ti
+                    heapq.heappush(min_time_heap, [node2time[dst], dst])
+
+                # add curr from seen
+        
+        max_time = 0
+        for node, time in node2time.items():
+            max_time = max(max_time, time)
+        
+        return max_time if max_time < math.inf else -1
+
+        
